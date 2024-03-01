83e3d2135970e910fbf452df9c8dadb6ee6d7579
Sanu Islam <dropnow.sanu@gmail.com>
2024-03-01 17:00:41 +0600

    Hot Topics Responsive

--- app/_components/HotTopics.tsx
+++ app/_components/HotTopics.tsx
@@ -1,5 +1,7 @@
+"use client";
 import { ArrowLeftCircleIcon, ArrowRightCircleIcon } from "lucide-react";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 type Props = {};
 
@@ -43,11 +45,39 @@ const Cards = [
 ];
 
 const HotTopics = (props: Props) => {
+  const [windowWidth, setWindowWidth] = useState(0);
+  const [slicedCards, setSlicedCards] = useState(6);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Add event listener to window resize
+    window.addEventListener('resize', handleResize);
+
+    // Initial window width
+    setWindowWidth(window.innerWidth);
+
+    // Cleanup function
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Slice cards based on window width
+    if (windowWidth <= 768) { // Change the value according to your breakpoint for mobile
+      setSlicedCards(1);
+    } else {
+      setSlicedCards(6);
+    }
+  }, [windowWidth, Cards]);
   return (
     <div className="mx-auto max-w-screen-xl">
       <div className="border mx-8 p-6 border-slate-300 shadow-lg bg-gradient-to-l from-slate-200 via-sky-50 to-slate-200 rounded-md shadow-teal-500/10">
-        <div className="grid grid-cols-7 gap-5">
-          <div className="space-y-4">
+        <div className="grid grid-cols-1 md:grid-cols-7 gap-5">
+          <div className="col-span-1 space-y-4">
             <h2 className="text-xl font-bold">Hot topics</h2>
             <p className="text-sm text-muted-foreground">
               Don't miss out on the latest news about Travel tips, Hotels
@@ -58,7 +88,7 @@ const HotTopics = (props: Props) => {
               <ArrowRightCircleIcon className="w-6 h-6 cursor-pointer" />
             </div>
           </div>
-          {Cards.map((card) => (
+          {Cards.slice(0, slicedCards).map((card) => (
             <div key={card.id} className="relative">
               <Image
                 alt="img"
@@ -69,7 +99,9 @@ const HotTopics = (props: Props) => {
               />
               <div className="absolute left-2 bottom-6">
                 <h3 className="font-bold text-slate-50">{card.name}</h3>
-                <p className="text-sm text-slate-200">Articles {card.articale}</p>
+                <p className="text-sm text-slate-200">
+                  Articles {card.articale}
+                </p>
               </div>
             </div>
           ))}
